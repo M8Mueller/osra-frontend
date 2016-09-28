a8ab4d9a600368fd73bd4e8e888e041c12e28b4c
Matthew Mueller <matthewmueller@dhcp34-gc5.genomecenter.ucdavis.edu>
2016-09-28 11:36:41 -0700

    initial commit with osra content

---
+++ app/directives.js
@@ -0,0 +1,92 @@
+/**
+ * Created by matthewmueller on 9/23/16.
+ */
+'use strict';
+
+
+angular
+
+
+    .module('myApp')
+
+
+    // Angular File Upload module does not include this directive
+    // Only for example
+
+
+    /**
+     * The ng-thumb directive
+     * @author: nerv
+     * @version: 0.1.2, 2014-01-09
+     */
+    .directive('ngThumb', ['$window', function($window) {
+        var helper = {
+            support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+            isFile: function(item) {
+                return angular.isObject(item) && item instanceof $window.File;
+            },
+            isImage: function(file) {
+                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|tiff|tif|'.indexOf(type) !== -1;
+            },
+            isTiff: function(file) {
+                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|tiff|tif|'.indexOf(type) !== -1;
+            }
+        };
+
+        return {
+            restrict: 'A',
+            template: '<canvas/>',
+            link: function(scope, element, attributes) {
+                if (!helper.support) return;
+
+                var params = scope.$eval(attributes.ngThumb);
+
+                if (!helper.isFile(params.file)) return;
+                if (!helper.isImage(params.file)) return;
+                if (helper.isTiff(params.file)) {
+                    console.log('This bad boy is a TIFF');
+                    //var canvas = element.find('canvas');
+
+                    var reader = new FileReader();
+
+                    reader.onload = function() {
+                        var buffer = this.result;
+                        var tiff = new Tiff({buffer: buffer});
+                        var canvas = tiff.toCanvas();
+                        var oldCanvas = element.find('canvas');
+                        var parent = oldCanvas[0].parentNode;
+                        console.log(parent);
+                        parent.removeChild(oldCanvas[0]);
+                        parent.appendChild(canvas);
+                        console.log(parent);
+                    };
+                    reader.readAsArrayBuffer(params.file);
+
+                } else {
+
+                    var canvas = element.find('canvas');
+                    var reader = new FileReader();
+
+                    reader.onload = onLoadFile;
+                    reader.readAsDataURL(params.file);
+
+                }
+
+                function onLoadFile(event) {
+                    var img = new Image();
+                    img.onload = onLoadImage;
+                    img.src = event.target.result;
+                }
+
+                function onLoadImage() {
+                    var width = params.width || this.width / this.height * params.height;
+                    var height = params.height || this.height / this.width * params.width;
+                    canvas.attr({ width: width, height: height });
+                    canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                }
+
+            }
+        };
+    }]);
---
+++ app/upload.js
@@ -0,0 +1,40 @@
+/**
+ * Created by matthewmueller on 9/22/16.
+ */
+'use strict';
+
+
+angular
+
+
+    .module('upload', ['angularFileUpload'])
+
+
+    .controller('UploadController', ['$scope', '$http', 'FileUploader', function($scope, $http, FileUploader) {
+        var uploader = $scope.uploader = new FileUploader({
+            //url: 'upload.php'
+        });
+
+        // CALLBACKS
+
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+
+            var data = new FormData();
+            data.append('file', fileItem._file);
+            data.append('file_format', fileItem.file.type);
+            $scope.item = uploader.queue[uploader.queue.length-1];
+            console.log($scope.item);
+            $http.post('http://localhost:8080/convert',data, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            }).then(function(response){
+                console.info(response);
+
+                $scope.MOLdata = response.data[0].outputLog.split("M  END")[0];
+            });
+            console.log(uploader);
+            var uploaderTop = $scope.uploaderTop = [ uploader.queue[uploader.queue.length-1] ];
+            console.log(uploaderTop);
+        };
+    }]);
